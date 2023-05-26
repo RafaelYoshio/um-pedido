f9507ba8e7271dc31a52c09bb39eacc0dc867588
RafaelYoshio <134745543+RafaelYoshio@users.noreply.github.com>
2023-05-26 20:26:24 -0300

    css/js 100%

---
+++ components/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.custom__container').addClass('active')
+})
+
+if ($(window).width() > 991) {
+    $('.custom__buttons--no').hover(function(){
+        $(this).css({"left": Math.random() * (window.outerWidth - 200) , "top": Math.random() * (window.outerHeight - 200), "position": "fixed", "background": "red", "color": "#fff"})
+    });
+} else {
+    $('.custom__buttons--no').click(function(){
+        $(this).css({"left": Math.random() * (window.outerWidth - 50) , "top": Math.random() * (window.outerHeight - 200), "position": "fixed", "background": "red", "color": "#fff"})
+    });
+}
